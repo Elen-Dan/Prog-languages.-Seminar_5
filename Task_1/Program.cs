cb0286670f7eba43624faedca8b3ce6bccff5a7f
El_Dan <lilit-li@ya.ru>
2022-09-05 20:19:15 +0400

    Решение задачи 1. Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задача 1:
+Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество 
+чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2
+*/
+Console.Clear();
+
+int Start = 100;
+int Stop = 999;
+int index = 0;
+int Sum = 0;
+
+int [] Array = new int[1000];
+// Заполнение массива
+while(index < Stop)
+    {
+        Array[index] = new Random().Next(Start, Stop);
+        Console.Write(Array[index] + " ");
+        if (Array[index] % 2 == 0)
+            Sum = Sum + Array[index]; //Находим количество четных чисел в массиве
+        index++;
+    }
+Console.WriteLine("");
+Console.WriteLine("Сумма четных чисел массива равна " + Sum);
